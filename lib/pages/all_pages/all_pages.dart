4cbaf8311edad30e619bc46b89f54d4f863f700c
Roger Bacab <roger.bacab7@gmail.com>
2024-03-05 14:05:42 -0600

    fixing clock page name

--- lib/pages/all_pages/all_pages.dart
+++ lib/pages/all_pages/all_pages.dart
@@ -1,4 +1,4 @@
 export '01_verification_code/verification_code_page.dart';
 export '02_coffe_maker/coffe_maker_page.dart';
 export '03_rgb_light/rgb_light_page.dart';
-export '04_flip_clock/flip_clock_padge.dart';
+export '04_flip_clock/flip_clock_page.dart';
